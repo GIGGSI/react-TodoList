b07c58baa186fad4e46ad637f401c05c9d84efc7
Stoyan Spasov <spasovstoyan@gmail.com>
2020-08-21 17:57:02 +0300

    create app

---
+++ src/component/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import TodoInput from './TodoInput'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <TodoInput />
+            </div>
+        )
+    }
+}
