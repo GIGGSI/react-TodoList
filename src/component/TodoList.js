6fcc7aaf9f2989fe82ab17337591f89da403f3ad
Stoyan Spasov <spasovstoyan@gmail.com>
2020-08-22 14:06:07 +0300

    fixing todoInput

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 export default class TodoList extends Component {
     render() {
         return (
             <div>
-                <TodoInput />
+                <TodoItem />
             </div>
         )
     }
